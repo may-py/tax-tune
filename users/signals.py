3cf1c867e6362da232d53a065f18cfad00a19921
may-py <65947362+may-py@users.noreply.github.com>
2021-03-02 15:10:29 +0700

    Update signals.py

--- users/signals.py
+++ users/signals.py
@@ -8,7 +8,10 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
-
+        
 @receiver(post_save, sender=User)
 def save_profile(sender, instance,  **kwargs):
-    instance.profile.save()
+    try:
+        instance.profile.save()
+    except ObjectDoesNotExist:
+        Profile.objects.create(user=instance)
