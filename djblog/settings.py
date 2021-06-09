32a4fe0be78313286fa3157e1be153ceddf92294
may-py <65947362+may-py@users.noreply.github.com>
2021-06-09 21:23:03 +0700

    commit

--- djblog/settings.py
+++ djblog/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY_djblog']
+SECRET_KEY = os.environ.get('SECRET_KEY_djblog')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','tax-truong.herokuapp.com']
+ALLOWED_HOSTS = ['tax-truong.herokuapp.com']
 
 # Application definition
 
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
