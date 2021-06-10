473d784f2cb715aaf32a2e74263adce554ce5bb8
may-py <65947362+may-py@users.noreply.github.com>
2021-06-10 21:31:44 +0700

    commit

--- djblog/settings.py
+++ djblog/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/blog/static/'
+STATIC_URL = '/static/'
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'blog/static')),
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'blog/static')),
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
